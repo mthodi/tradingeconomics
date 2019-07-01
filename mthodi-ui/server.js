409c23d7673bd4c87099b2f5300633f79b7f5172
Martin Thodi <martthodi238@gmail.com>
2019-07-01 14:03:37 +0200

    Added server.js for to run build

--- mthodi-ui/server.js
+++ mthodi-ui/server.js
@@ -3,11 +3,11 @@ var path = require('path');
 var serverStatic = require('serve-static');
 //var history = require('connect-history-api-fallback');
 
-app = express()
+app = express();
 app.use(serverStatic(path.join(__dirname, 'dist')));
 //app.use(history());
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 5002;
 app.listen(port);
 
-console.log("server running on: " + port)
+console.log("server running on: http://127.0.0.1:" + port)
